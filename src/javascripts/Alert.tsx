50ec097215bbcea61d0763f57cfb141304123663
YukiKaburagi <yuki.loves.omurice@gmail.com>
2020-08-08 15:05:57 +0900

    Install Typescript

---
+++ src/javascripts/Alert.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+const Alert: React.FC<{ message: string}> = ({message}) => {
+  return(
+    <div style={{backgroundColor: "green", color: "white", padding: "1em"}}>
+      {message}
+    </div>
+  )
+}
+
+export default Alert;
